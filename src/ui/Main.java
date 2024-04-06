75cf6a8befe29fa4af06a7fe4a51d4b4b460fb40
juanO <juancamilocorra@gmail.com>
2024-04-06 11:37:31 -0500

    added implementation

---
+++ src/ui/Main.java
@@ -0,0 +1,11 @@
+package ui;
+
+import facade.VideoConversionFacade;
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        VideoConversionFacade converter = new VideoConversionFacade();
+        File mp4Video = converter.convertVideo("youtubevideo.ogg", "mp4");
+    }
+}
